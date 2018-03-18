c03b389302ebdf5df214db2df22da1f0960151e2
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-03-18 11:32:42 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,13 +1,5 @@
 const { events } = require('brigadier')
 
-events.on("Microsoft.Storage.BlobDeleted", (e, p) => {
-  console.log(e)
-})
-
-events.on("Microsoft.Storage.BlobCreated", (e, p) => {
-  console.log(e)
-})
-
-events.on("Microsoft.EventGrid.SubscriptionValidationEvent", (e, p) => {
+events.on("Microsoft.Resources.ResourceWriteSuccess", (e, p) => {
   console.log(e)
 })
