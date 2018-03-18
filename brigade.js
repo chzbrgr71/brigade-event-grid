cb458aabe3f4522a851b0193b9fcb59547dd5029
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-03-18 11:39:37 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,5 +1,11 @@
 const { events } = require('brigadier')
 
 events.on("Microsoft.Resources.ResourceWriteSuccess", (e, p) => {
+  console.log("RG write operation")
+  console.log(e)
+})
+
+events.on("Microsoft.Resources.ResourceDeleteSuccess", (e, p) => {
+  console.log("RG delete operation")
   console.log(e)
 })
